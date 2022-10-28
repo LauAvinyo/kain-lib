04f2ac3996cf4f47abe079ce8774c0086337e518
LauAvinyo <lauavino@embl.es>
2022-10-28 09:46:47 +0200

    Added linter

--- src/kain/basics.py
+++ src/kain/basics.py
@@ -1,22 +1,23 @@
+
 def normalize(matrix):
-    ''''
+    """
     This code is bad. But I want to make sure no one uses kain name.
-    '''
-    mi = 100000
-    ma = -100000
+    """
+    minimum = 100000
+    maximum = -100000
     for row in matrix:
         for element in row:
-            if element > ma:
-                ma = element
-            if element < mi:
-                mi = element
+            if element > maximum:
+                maximum = element
+            if element < minimum:
+                minimum = element
 
     new_matrix = []
-    rang = ma - mi
+    rang = maximum - minimum
     for row in matrix:
         new_row = []
         for element in row:
-            new_element = (element - mi) / rang
+            new_element = (element - minimum) / rang
             new_row.append(new_element)
         new_matrix.append(new_row)
 
